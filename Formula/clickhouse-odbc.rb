741b4f56f765aa974895922e894e6486b4cd2339
shifshuf <45665877+shifshuf@users.noreply.github.com>
2020-04-15 16:32:25 +0300

    Create clickhouse-odbc.rb

---
+++ Formula/clickhouse-odbc.rb
@@ -0,0 +1,37 @@
+class ClickhouseOdbc < Formula
+  desc "ClickHouse ODBC driver"
+  homepage "https://clickhouse.yandex"
+  url "https://github.com/yandex/clickhouse-odbc.git",
+      :tag      => "v1.1.7.20200414",
+      :revision => "2032dad9646670e28daf05529a77aff96ae5d740"
+
+  depends_on "cmake" => :build
+  depends_on "libiodbc" => :build
+  depends_on "libtool" => :build
+  depends_on "ninja" => :build
+  depends_on "openssl" => :build
+
+  def install
+    args = std_cmake_args
+    mkdir "build" do
+      system "cmake", "-G", "Ninja", "..", *args
+      system "cmake", "--build", "."
+      system "ctest", "-V"
+      # system "ninja", "install" # installs too many trash
+      lib.install "driver/libclickhouseodbc.dylib"
+      lib.install "driver/libclickhouseodbcw.dylib"
+    end
+    lib.install_symlink "#{lib}/libclickhouseodbc.dylib" => "libclickhouseodbc.so"
+    lib.install_symlink "#{lib}/libclickhouseodbcw.dylib" => "libclickhouseodbcw.so"
+  end
+
+  test do
+    assert_match "/usr/lib/libSystem.B.dylib", shell_output("otool -L #{lib}/libclickhouseodbc.so")
+    assert_match "/usr/lib/libSystem.B.dylib", shell_output("otool -L #{lib}/libclickhouseodbcw.so")
+    # TODO: fix build with unixodbc and enable:
+    # output = shell_output("#{Formula["unixodbc"].opt_bin}/dltest #{lib}/libclickhouseodbc.so")
+    # assert_equal "SUCCESS: Loaded #{lib}/libclickhouseodbc.so\n", output
+    # output = shell_output("#{Formula["unixodbc"].opt_bin}/dltest #{lib}/libclickhouseodbcw.so")
+    # assert_equal "SUCCESS: Loaded #{lib}/libclickhouseodbcw.so\n", output
+  end
+end
